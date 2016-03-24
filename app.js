ef166a838cde150349938b5878af4446394a663f
Logan Marshall <wildmovementco@gmail.com>
2016-03-24 13:46:51 -0400

    Made site redesign mobile responsive

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 $(document).ready(function(){
     $(this).scrollTop(0);
     resizeDiv();
-    $(".hamburger-btn i").click(function() {
+    $("#navicon").click(function() {
         console.log('ran function!')
         $(".dropdown").slideToggle();
     })
